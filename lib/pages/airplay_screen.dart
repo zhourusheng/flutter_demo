661e13b41f7dd9a2496db29963769caef39dd3a5
zhourusheng <nfuzrs@126.com>
2019-11-05 14:02:47 +0800

    头像层叠

--- lib/pages/airplay_screen.dart
+++ lib/pages/airplay_screen.dart
@@ -3,10 +3,37 @@ import 'package:flutter/material.dart';
 class AirplayScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Stack
+    var stack = new Stack(
+      alignment: FractionalOffset(0.5, 0.8),
+      children: <Widget>[
+        // CircleAvatar
+        new CircleAvatar(
+          backgroundImage: new NetworkImage('http://hbimg.b0.upaiyun.com/0b385b069e6184747f51bcfcde8abe77d8e901ee12d2-vkE7SV_fw658'),
+          radius: 100.0,
+        ),
+        new Positioned(
+          top: 10.0,
+          left: 10.0,
+          child: new Container(
+            decoration: new BoxDecoration(
+              color: Colors.lightBlue,
+            ),
+            padding: EdgeInsets.all(5.0),
+            child: new Text('可爱的头像嘿'),
+          ),
+        )
+      ],
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text('AirPlay'),
       ),
+      body: Center(
+        child: stack,
+      ),
+
       // Row 水平布局、Expanded灵活水平布局
       // body: new Row(
       //   children: <Widget>[
@@ -33,17 +60,19 @@ class AirplayScreen extends StatelessWidget {
 
 
       // Column 垂直方向布局
-      body: Column(
-        // 主轴上对齐方式, 类似于 Flex布局
-        mainAxisAlignment: MainAxisAlignment.center,
-        // 交叉轴上对齐方式
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text('黑黑黑黑'),
-          Text('哈哈哈哈这一行超级长唱唱唱'),
-          Text('2333333'),
-        ],
-      ),
+      // body: Column(
+      //   // 主轴上对齐方式, 类似于 Flex布局
+      //   mainAxisAlignment: MainAxisAlignment.center,
+      //   // 交叉轴上对齐方式
+      //   crossAxisAlignment: CrossAxisAlignment.center,
+      //   children: <Widget>[
+      //     Text('黑黑黑黑11'),
+      //     Text('哈哈哈哈这一行超级长唱唱唱'),
+      //     Text('2333333'),
+      //   ],
+      // ),
+
+
     );
   }
 }
