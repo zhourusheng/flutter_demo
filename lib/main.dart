254e3d1becf4b6653ffd597c1f7fe350d28980a7
zhourusheng <nfuzrs@126.com>
2019-11-06 11:03:39 +0800

    layout

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue
+      ),
       // home: new RandomWords(),
       home: BottomNavigationWidget(),
     );
--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -4,25 +4,95 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // 标题行
     Widget titleSection = new Container(
       padding: const EdgeInsets.all(32.0),
       child: new Row(
         children: <Widget>[
-
+          new Expanded(
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Container(
+                  padding: EdgeInsets.only(bottom: 8.0),
+                  child: new Text('Oeschinen Lake Campground',
+                      style: new TextStyle(fontWeight: FontWeight.bold)),
+                ),
+                new Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(color: Colors.grey[500]),
+                ),
+              ],
+            ),
+          ),
+          new Icon(Icons.star, color: Colors.red[500]),
+          new Text('41')
         ],
       ),
     );
 
-    return Scaffold(
-      body: new ListView(
+    // 构建 buildButtonColumn
+    Column buildButtonColumn(IconData icon, String label) {
+      Color color = Theme.of(context).primaryColor;
+
+      return new Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          new Image.asset(
-            'assets/images/bg.png',
-            height: 240,
-            fit: BoxFit.cover,
+          new Icon(
+            icon,
+            color: color,
+          ),
+          new Container(
+            margin: EdgeInsets.only(top: 8.0),
+            child: new Text(
+              label,
+              style: new TextStyle(
+                fontSize: 12.0,
+                fontWeight: FontWeight.bold,
+                color: color
+              )
+            ),
           )
         ],
-      )
+      );
+    }
+
+    // 按钮行
+    Widget buttonSection = new Container(
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          buildButtonColumn(Icons.call, 'Call'),
+          buildButtonColumn(Icons.near_me, 'ROUTE'),
+          buildButtonColumn(Icons.share, 'SHARE'),
+        ],
+      ),
     );
+
+    // 文本行
+    Widget textSection = new Container(
+      padding: EdgeInsets.all(32.0),
+      child: new Text(
+        '''
+Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.
+        ''',
+        softWrap: true,
+      ),
+    );
+
+    return Scaffold(
+        body: new ListView(
+      children: <Widget>[
+        new Image.asset(
+          'assets/images/bg.png',
+          height: 240,
+          fit: BoxFit.cover,
+        ),
+        titleSection,
+        buttonSection,
+        textSection
+      ],
+    ));
   }
 }
